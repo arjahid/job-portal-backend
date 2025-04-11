7714e06511e29950bf5b55377475b39fe2a9e0a3
arjahid <jhjahidhasanar@gmail.com>
2025-04-11 12:15:39 +0600

    added

--- index.js
+++ index.js
@@ -41,7 +41,12 @@ async function run() {
     app.post('/jwt',async(req,res)=>{
       const user=req.body;
       const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
-      res.send({token});
+      res
+      .cookie('token',token,{
+        httpOnly:true,
+        secure: false,
+      })
+      .send({token});
     })
 
     app.get('/jobs',async(req,res)=>{
